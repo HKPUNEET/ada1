c35d49d5208eb9a19f980a6616a8ffbcb3e51887
H K Puneet <gowdakempe4@gmail.com>
2026-08-02 23:12:04 +0530

    Update plotter function to include best/worst case output
    Modified the plotter function to output best and worst case counts for DFS on graphs of varying sizes. Updated the output format to include best and worst case data.

--- 20_topo_dfs.c
+++ 20_topo_dfs.c
@@ -42,11 +42,14 @@ void plotter()
 {
     FILE *fp = fopen("dfstopo.txt", "w");
     srand(time(NULL));
+
     for (n = 4; n <= 12; n++)
     {
+        /* ---------- BEST CASE : empty graph (no edges) ---------- */
         for (int i = 0; i < n; i++)
             for (int j = 0; j < n; j++)
-                A[i][j] = (i < j) ? rand() % 2 : 0;
+                A[i][j] = 0;
+
         for (int i = 0; i < n; i++)
             visited[i] = 0;
         top = -1;
@@ -54,16 +57,32 @@ void plotter()
         for (int i = 0; i < n; i++)
             if (!visited[i])
                 dfs(i);
-        fprintf(fp, "%d %d\n", n, count);
+        int best = count;
+
+        /* ---------- WORST CASE : complete DAG (all forward edges) ---------- */
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < n; j++)
+                A[i][j] = (i < j) ? 1 : 0;
+
+        for (int i = 0; i < n; i++)
+            visited[i] = 0;
+        top = -1;
+        count = 0;
+        for (int i = 0; i < n; i++)
+            if (!visited[i])
+                dfs(i);
+        int worst = count;
+
+        fprintf(fp, "%d %d %d\n", n, best, worst);
     }
     fclose(fp);
-    printf("Data written.\n");
+    printf("Data written (n best worst).\n");
 }
 
 int main()
 {
     int ch;
-    printf("1.Tester  2.Plotter\nChoice: ");
+    printf("1.Tester 2.Plotter\nChoice: ");
     scanf("%d", &ch);
     if (ch == 1)
         tester();
